73f7de59b0c58fa8947c5b691bdfe1f35dc1fb53
Henri De Boever <deboeverhenri@gmail.com>
2024-11-22 16:53:39 +0100

    re

--- components/Hashtag.js
+++ components/Hashtag.js
@@ -1,27 +1,38 @@
-import styles from '../styles/Hashtag.module.css';
-import Head from 'next/head';
-import Tweet from './Tweet'
-import Trends from './Trends';
-import Home from './Home';
-import ReactDOM from 'react-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faHeart } from '@fortawesome/free-solid-svg-icons'
-import { faTwitter } from '@fortawesome/free-brands-svg-icons'
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { login, logout } from '../reducers/user'
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout } from '../reducers/user';
+import styles from '../styles/Hashtag.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import Trends from '../components/Trends'
 
 function Hashtag() {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   const [tweets, setTweets] = useState([]);
+  const dispatch = useDispatch();
+  const router = useRouter(); 
+
+  const user = useSelector((state) => state.user.value);
+
+  useEffect(() => {
+    if (router.query.tag) {
+      const tag = router.query.tag.startsWith('#')
+        ? router.query.tag
+        : `#${router.query.tag}`;
+      setInput(tag);
+      fetchData(tag);
+    }
+  }, [router.query.tag]);
 
   const fetchData = (value) => {
     fetch(`http://localhost:3000/tweet/allTweet`)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         const filteredTweets = data.tweets.filter((tweet) =>
-          tweet.content.toLowerCase().includes(value)
+          tweet.content.toLowerCase().includes(value.toLowerCase())
         );
         setTweets(filteredTweets);
       });
@@ -32,55 +43,82 @@ function Hashtag() {
     fetchData(value);
   };
 
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user.value);
-  console.log(user.username)
   const handleLogout = () => {
-    dispatch(logout())
-    return
-  }
+    dispatch(logout());
+    return;
+  };
 
   return (
     <div className={styles.main}>
-
       <div className={styles.left}>
         <div className={styles.logoTwitter}>
-          <FontAwesomeIcon icon={faTwitter} rotation={180} style={{ color: "#ffffff", }} className={styles.twitter} />
+          <FontAwesomeIcon
+            icon={faTwitter}
+            rotation={180}
+            style={{ color: '#ffffff' }}
+            className={styles.twitter}
+          />
         </div>
         <div className={styles.userInfo}>
           <div className={styles.userLogo}>
-            <FontAwesomeIcon icon={faUser} style={{ color: "#ffffff", }} className={styles.userImg} />
+            <FontAwesomeIcon
+              icon={faUser}
+              style={{ color: '#ffffff' }}
+              className={styles.userImg}
+            />
           </div>
           <div>
             <p>{user.firstname}</p>
             <p>@{user.username}</p>
-            <Link href='/login'><button onClick={() => handleLogout()} type='button'>Logout</button></Link>
-          </div></div>
+            <Link href="/login">
+              <button onClick={() => handleLogout()} type="button">
+                Logout
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
 
       <hr />
 
-
       <div className={styles.middle}>
-        <h2 className={styles.home}> Hashtag </h2>
-        <input type='text' placeholder="Search" className={styles.writeTweet} maxLength='280' value={input}
-          onChange={(e) => handleChange(e.target.value.startsWith('#') ? e.target.value : `#${e.target.value}`)} />
+        <h2 className={styles.home}>Hashtag</h2>
+        <input
+          type="text"
+          placeholder="Search"
+          className={styles.writeTweet}
+          maxLength="280"
+          value={input}
+          onChange={(e) =>
+            handleChange(
+              e.target.value.startsWith('#') ? e.target.value : `#${e.target.value}`
+            )
+          }
+        />
         <hr />
         {tweets.map((tweet) => (
           <div key={tweet._id}>
             <div className={styles.lastTweet}>
               <div className={styles.userInfo2}>
-                <FontAwesomeIcon icon={faUser} style={{ color: "#ffffff", }} className={styles.icone} />
+                <FontAwesomeIcon
+                  icon={faUser}
+                  style={{ color: '#ffffff' }}
+                  className={styles.icone}
+                />
                 <div className={styles.typo}>
-                  <p className={styles.name}>{tweet.user.firstname} </p>
-                  <p className={styles.infos}> @{tweet.user.username}</p>
+                  <p className={styles.name}>{tweet.user.firstname}</p>
+                  <p className={styles.infos}>@{tweet.user.username}</p>
                   <p className={styles.infos}>2 hours</p>
                 </div>
               </div>
               <div>
                 <p className={styles.text}>{tweet.content}</p>
                 <div className={styles.likes}>
-                  <FontAwesomeIcon icon={faHeart} style={{ color: "#ffffff", }} className={styles.heart} />
+                  <FontAwesomeIcon
+                    icon={faHeart}
+                    style={{ color: '#ffffff' }}
+                    className={styles.heart}
+                  />
                   <p className={styles.counter}>{tweet.likes.length}</p>
                 </div>
               </div>
@@ -88,19 +126,14 @@ function Hashtag() {
             <hr />
           </div>
         ))}
-
       </div>
 
       <hr />
 
       <div className={styles.right}>
-
         <Trends />
       </div>
-
     </div>
-
-
   );
 }
 
--- components/Trends.js
+++ components/Trends.js
@@ -1,26 +1,65 @@
 import styles from '../styles/Trends.module.css'
+import { useEffect, useState } from 'react'
+import Link from 'next/link';
 
-function Trends () {
+function Trends() {
+  const [trends, setTrends] = useState([]);
+  const [hashtags, setHashtags] = useState([]);
 
-    return (
-        <div className={styles.trends}>
-        <h2 className={styles.title}> Trends </h2>
-        <div className={styles.hashtags}> 
-            <div className={styles.bloc}>
-                <p className={styles.text}> #lospolloshermanos</p>
-                <p className={styles.counterHash}>hashtags.length Tweets</p>
-            </div>
-            <div className={styles.bloc}>
-                <p className={styles.text}> #lospolloshermanos</p>
-                <p className={styles.counterHash}>hashtags.length Tweets</p>
-            </div>
-            <div className={styles.bloc}>
-                <p className={styles.text}> #lospolloshermanos</p>
-                <p className={styles.counterHash}>hashtags.length Tweets</p>
-            </div>
-        </div>
-        </div>
-    )
+  useEffect(() => {
+    fetch('http://localhost:3000/tweet/allTweet')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch trends');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setTrends(data.tweets);
+      })
+      .catch((error) => {
+        console.error('Error fetching trends:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const hashtagMap = new Map();
+
+    trends.forEach((tweet) => {
+      const hashtagsInTweet = tweet.content.match(/#\w+/g);
+
+      if (hashtagsInTweet) {
+        hashtagsInTweet.forEach((hashtag) => {
+          const lowercaseHashtag = hashtag.toLowerCase();
+          hashtagMap.set(
+            lowercaseHashtag,
+            (hashtagMap.get(lowercaseHashtag) || 0) + 1
+          );
+        });
+      }
+    });
+
+    const sortedHashtags = Array.from(hashtagMap.entries()).sort(
+      (a, b) => b[1] - a[1]
+    );
+    setHashtags(sortedHashtags);
+  }, [trends]);
+
+  return (
+    <div className={styles.trends}>
+      <h2 className={styles.title}>Trends</h2>
+      <div className={styles.hashtags}>
+        {hashtags.map(([hashtag, count]) => (
+          <div className={styles.bloc} key={hashtag}>
+            <Link href={`/hashtag?tag=${encodeURIComponent(hashtag)}`}>
+              <a className={styles.text}>{hashtag}</a>
+            </Link>
+            <p className={styles.counterHash}>{count}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Trends
+export default Trends;
